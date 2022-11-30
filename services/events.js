f794efe93d19f8ff9b7a17bbe22648da01f62912
Jason Leo <30601823+xezian@users.noreply.github.com>
2022-11-30 11:49:02 -0500

    moves event.json

--- services/events.js
+++ services/events.js
@@ -1,51 +1,53 @@
-
 const loopText = (text) => {
-  if (!text || !text.length) return
+  if (!text || !text.length) return;
   let rtnStr = "";
-  text.forEach(one => {
-    rtnStr += `<br/>${one}`
-  })
-  return rtnStr
-}
-
+  text.forEach((one) => {
+    rtnStr += `<br/>${one}`;
+  });
+  return rtnStr;
+};
 
 const populateUpcomingEvents = async () => {
-  const response = await fetch("../events.json")
+  const response = await fetch("./events.json");
   const events = await response.json();
-  const place = document.getElementById("upcoming-events")
-  if (!place) return
-  events.forEach(event => {
-    const para = document.createElement('p');
-    const eventDate = new Date(event.date)
+  const place = document.getElementById("upcoming-events");
+  if (!place) return;
+  events.forEach((event) => {
+    const para = document.createElement("p");
+    const eventDate = new Date(event.date);
     if (eventDate >= new Date()) {
       para.innerHTML = `
-        <b>${eventDate.getMonth() + 1}/${eventDate.getDate() + 1} - ${event.title}</b>
+        <b>${eventDate.getMonth() + 1}/${eventDate.getDate() + 1} - ${
+        event.title
+      }</b>
         ${loopText(event.text) || ""}
         <br/><br/><br/>
-      `
-      place.appendChild(para)
+      `;
+      place.appendChild(para);
     }
-  })
-}
+  });
+};
 
 const populatePastEvents = async () => {
-  const response = await fetch("../events.json")
+  const response = await fetch("./events.json");
   const events = await response.json();
-  const place = document.getElementById("past-events")
-  if (!place) return
-  events.forEach(event => {
-    const para = document.createElement('p');
-    const eventDate = new Date(event.date)
+  const place = document.getElementById("past-events");
+  if (!place) return;
+  events.forEach((event) => {
+    const para = document.createElement("p");
+    const eventDate = new Date(event.date);
     if (eventDate < new Date()) {
       para.innerHTML = `
-        <b>${eventDate.getMonth() + 1}/${eventDate.getDate() + 1} - ${event.title}</b>
+        <b>${eventDate.getMonth() + 1}/${eventDate.getDate() + 1} - ${
+        event.title
+      }</b>
         ${loopText(event.text) || ""}
         <br/><br/><br/>
-      `
-      place.appendChild(para)
+      `;
+      place.appendChild(para);
     }
-  })
-}
+  });
+};
 
-populateUpcomingEvents()
-populatePastEvents()
+populateUpcomingEvents();
+populatePastEvents();
