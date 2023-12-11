40f83372d8ec37d4628a631b39dadff8d6dfcd24
Jason A Leo <30601823+xezian@users.noreply.github.com>
2023-12-11 14:59:19 -0500

    Improve the placement look and behavior of gift card modal

--- services/gift-card-modal.js
+++ services/gift-card-modal.js
@@ -8,14 +8,18 @@ async function openModal () {
   document.body.style.top =  "-" + position + "px";
   document.body.style.position = "fixed";
   document.body.style.width = "100%";
-  document.body.style.overflowY = "scroll"
-  gcm.style.display = "grid";
+  document.body.style.overflowY = "scroll";
+  gcm.style.height = "100vh";
+  gcm.style.opacity = 1;
   gcm.innerHTML = await (await fetch("gift-card-modal.html")).text();
-  gcm.addEventListener("click", () => {
+  closeButton = document.getElementById("closeGiftCardModal")
+  closeButton.addEventListener("click", () => {
     document.body.style.position = "static";
     document.body.style.overflow = "auto"
     document.documentElement.scrollTop = position;
-    gcm.style.display = "none";
+    gcm.style.opacity = 0;
+    gcm.style.height = 0;
+    gcm.innerHTML = null;
   })
 }
 
